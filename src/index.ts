500473a50b59e51310bb4505b83455f53345c81c
Jason Sherrick <jasonsherrick@gmail.com>
2024-06-24 00:16:07 -0500

    why is parcel being a piece of shit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-import 'dotenv';
+import 'dotenv/config';
+import { User } from './models/User';
+
+const mongoUser = process.env.mongoUser;
+const mongoDb = process.env.mongoDb;
+const mongoPass = process.env.mongoPw;
+const mongoPort = process.env.mongoPort;
+
+const uri = `mongodb://${mongoUser}:${mongoPass}@localhost:${mongoPort}/${mongoDb}?authSource=${mongoDb}`;
+
+console.log(uri);
